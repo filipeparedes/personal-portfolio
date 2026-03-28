055f7a3ddd3216215cf265a1e9c4e6aa85e8c573
filipeparedes <filipeparedes3@gmail.com>
2026-03-28 23:25:17 +0000

    feat: setup api route
    + Created initial /api/about route with personal data.
    + Fixed port conflict by moving from 5001 to 5005.

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -3,8 +3,28 @@ import { Router } from "express";
 const router = Router();
 
 // api endpoint for testing
-router.get('/', (req, res) => {
-    res.json({ message: 'Hello from the API!' });
+router.get('/about', (req, res) => {
+    res.json({ 
+        name: 'Filipe Paredes',
+        role: 'Junior Software Engineer',
+        techStack: [
+            'JavaScript', 
+            'TypeScript', 
+            'React', 'Node.js', 
+            'Express', 
+            'CSS', 
+            'HTML',
+            'Git',
+            'SQL',
+            'C',
+            'C++',
+            'C#',
+            'Java',
+            'Python',
+            'Azure'
+        ],
+        description: "Welcome to my personal portfolio!"
+     });
 });
 
 export default router;
