6c6436f4e1025f8fad75cffd03206294f54dc8fd
filipeparedes <filipeparedes3@gmail.com>
2026-04-01 22:05:15 +0100

    feat: implement footer design
    + Implemented footer layout and design, according with Figma mockups.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,30 @@
+import { Heart } from "lucide-react";
+import {CodeRow as C} from "./ui/CodeRow";
+
 export function Footer() {
-    return (
-    <section className="h-[600px] bg-slate-900 flex items-center justify-center border-b border-slate-800">
-      <h2 className="text-3xl text-slate-700 font-mono">{"<Footer />"}</h2>
-    </section>
+  return (
+    <footer className="py-8 bg-slate-950 border-t border-slate-800">
+      <div className="container mx-auto px-6">
+        <div className="text-center">
+          {/*const madeWith = <3 + React + TypeScript; */}
+          <p className="font-mono mb-2">
+            <C type="keyword">const</C>{" "}
+            <C type="var">madeWith</C>{" "}
+            <C type="comment">=</C>{" "}
+            <span className="inline-flex items-center gap-1">
+              <Heart className="w-4 h-4 text-red-500 fill-red-500 inline" />
+            </span>{" "}
+            <C type="comment">+</C>{" "}
+            <C type="string">"React"</C>{" "}
+            <C type="comment">+</C>{" "}
+            <C type="string">"TypeScript"</C>
+            <C type="comment">;</C>
+          </p>
+          <C type="comment">
+            © 2026 Filipe Paredes | Todos os direitos reservados
+          </C>
+        </div>
+      </div>
+    </footer>
   );
 }
